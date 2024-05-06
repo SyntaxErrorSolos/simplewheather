3ef2afede71b7ee0d087d9c13b8110544b8a4c4a
Huzaifa <68179601+SyntaxErrorSolos@users.noreply.github.com>
2024-05-06 23:27:37 +0500

    wheather

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,10 @@
-import { Inter } from "next/font/google";
+import { Lexend } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const lexend = Lexend({
+  subsets: ["latin"],
+  weight: ["100", "300", "500", "700"],
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={lexend.className}>{children}</body>
     </html>
   );
 }
